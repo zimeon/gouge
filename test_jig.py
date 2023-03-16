500c57376b24306be48c4bf8e9e36c54d256885c
Simeon Warner <simeon.warner@cornell.edu>
2023-03-15 20:03:35 -0400

    Fix test

--- test_jig.py
+++ test_jig.py
@@ -1,6 +1,8 @@
 """Tests for GrindingJig."""
 import unittest
 import math
+import numpy
+import numpy.testing as npt
 
 from gouge import GrindingJig
 
@@ -15,18 +17,10 @@ class TestJig(unittest.TestCase):
         j.length = 1.0
         j.angle = math.radians(30.0)
         # Centered, rotation=0.0
-        ex, ey, ez, tx, ty, tz = j.tool_vectors(rotation=0.0)
-        self.assertAlmostEqual(ex, 0.0)
-        self.assertAlmostEqual(ey, 0.5, 3)
-        self.assertAlmostEqual(ez, 0.0)
-        self.assertAlmostEqual(tx, 0.866, 3)
-        self.assertAlmostEqual(ty, 0.0)
-        self.assertAlmostEqual(tz, 0.0)
+        x, y = j.tool_vectors(rotation=0.0)
+        npt.assert_allclose(x, numpy.array([0.0, 0.5, 0.0]), atol=0.001)
+        npt.assert_allclose(y, numpy.array([0.866, 0.0, 0.0]), atol=0.001)
         # All the way over, rotation=90.0
-        ex, ey, ez, tx, ty, tz = j.tool_vectors(rotation=math.radians(90.0))
-        self.assertAlmostEqual(ex, 0.217, 3)
-        self.assertAlmostEqual(ey, 0.125, 3)
-        self.assertAlmostEqual(ez, 0.433, 3)
-        self.assertAlmostEqual(tx, 0.650, 3)
-        self.assertAlmostEqual(ty, 0.375, 3)
-        self.assertAlmostEqual(tz, -0.433, 3)
+        x, y = j.tool_vectors(rotation=math.radians(90.0))
+        npt.assert_allclose(x, numpy.array([0.217, 0.125, 0.433]), atol=0.001)
+        npt.assert_allclose(y, numpy.array([0.650, 0.375, -0.433]), atol=0.001)
