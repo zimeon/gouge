1f7cb9b1da07fb6ecc29c61fa5d079f4589c48cb
Simeon Warner <simeon.warner@cornell.edu>
2023-03-20 13:04:12 -0400

    Fix tests for new jig coords

--- test_jig.py
+++ test_jig.py
@@ -17,13 +17,13 @@ class TestJig(unittest.TestCase):
         j.length = 1.0
         j.angle = math.radians(30.0)
         # Centered, rotation=0.0
-        x, y = j.tool_vectors(rotation=0.0)
-        npt.assert_allclose(x, numpy.array([0.0, 0.5, 0.0]), atol=0.001)
-        npt.assert_allclose(y, numpy.array([0.866, 0.0, 0.0]), atol=0.001)
+        y, z = j.tool_vectors(rotation=0.0)
+        npt.assert_allclose(y, numpy.array([0.0, 1.0, 0.0]), atol=0.001)
+        npt.assert_allclose(z, numpy.array([0.0, 0.0, 1.0]), atol=0.001)
         # All the way over, rotation=90.0
-        x, y = j.tool_vectors(rotation=math.radians(90.0))
-        npt.assert_allclose(x, numpy.array([0.217, 0.125, 0.433]), atol=0.001)
-        npt.assert_allclose(y, numpy.array([0.650, 0.375, -0.433]), atol=0.001)
+        y, z = j.tool_vectors(rotation=math.radians(90.0))
+        npt.assert_allclose(y, numpy.array([-0.866, 0.250, 0.433]), atol=0.001)
+        npt.assert_allclose(z, numpy.array([0.500, 0.433, 0.750]), atol=0.001)
 
     def test03_tool_rotation_matrix(self):
         j = Jig()
@@ -31,11 +31,11 @@ class TestJig(unittest.TestCase):
         j.angle = math.radians(30.0)
         # Centered, rotation=0.0
         r = j.tool_rotation_matrix(rotation=0.0)
-        npt.assert_allclose(r, numpy.array([[0.0, 0.0, 1.0],
+        npt.assert_allclose(r, numpy.array([[1.0, 0.0, 0.0],
                                             [0.0, 1.0, 0.0],
-                                            [-1.0, 0.0, 0.0]]), atol=0.001)
+                                            [0.0, 0.0, 1.0]]), atol=0.001)
         # All the way over, rotation=90.0
         r = j.tool_rotation_matrix(rotation=math.radians(90.0))
-        npt.assert_allclose(r, numpy.array([[-0.500, 0.433, 0.750],
+        npt.assert_allclose(r, numpy.array([[0.000, -0.866, 0.500],
                                             [0.866, 0.250, 0.433],
-                                            [0.000, 0.866, -0.500]]), atol=0.001)
+                                            [-0.500, 0.433, 0.750]]), atol=0.001)
